e67693a6d9a05a6d51c5038589fc8be0f1e2c094
xam4lor <xam4lor@gmail.com>
2019-12-30 20:54:07 +0100

    Added warning, info and danger panels

--- public/js/article_reformat.js
+++ public/js/article_reformat.js
@@ -1,5 +1,21 @@
 // RENDU DU CODE
 let converter = new showdown.Converter();
 function computeText(rawText) {
-    return converter.makeHtml(rawText);
+    return converter.makeHtml(handleBalises(rawText));
+}
+
+
+function handleBalises(rawText) {
+    let formattedText = rawText
+        .replace(/<\/panel>/g, '</p></div>')
+        .replace(/<panel type=info>/g,    getPanelHtml('info'))
+        .replace(/<panel type=warning>/g, getPanelHtml('warning'))
+        .replace(/<panel type=danger>/g,  getPanelHtml('danger'))
+    ;
+
+    return formattedText;
+}
+
+function getPanelHtml(type) {
+    return `<div class="panel-m panel-${type}-m"><p>`;
 }
