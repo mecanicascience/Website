f2d45e701b6dbd93e818791ee8ff7621f4d085d4
xam4lor <xam4lor@gmail.com>
2019-12-31 19:27:49 +0100

    Oops

--- routes/index.js
+++ routes/index.js
@@ -100,8 +100,9 @@ router
         else {
             let datas = m.articles.getArticleDatas(articleExists);
             let article = await m.articles.getArticlesForSuggestions(datas.category_id, 3, datas.uuid);
+            console.log(datas, datas.formatted_dates);
             try {
-                console.log(datas.formatted_dates);
+
                 datas.formatted_dates = datas.formatted_dates.replace(/\/.*\//, '/' + (
                     (parseInt(datas.formatted_dates.split('/')[1]) + 1) > 9 ?
                     (parseInt(datas.formatted_dates.split('/')[1]) + 1) + "" :
