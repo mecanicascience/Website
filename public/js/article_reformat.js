6e5fa481cb561c7a9efe886fa34a3769116ca92e
xam4lor <xam4lor@gmail.com>
2019-12-31 23:48:31 +0100

    Added custom iframe for animations

--- public/js/article_reformat.js
+++ public/js/article_reformat.js
@@ -6,6 +6,7 @@ function computeText(rawText) {
 
 
 function handleBalises(rawText) {
+    // PANELS
     let formattedText = rawText
         .replace(/<\/panel>/g, '</p></div>')
         .replace(/<panel type=info>/g,    getPanelHtml('info'))
@@ -13,9 +14,35 @@ function handleBalises(rawText) {
         .replace(/<panel type=danger>/g,  getPanelHtml('danger'))
     ;
 
+
+    // IFRAME
+    while(formattedText.search(/<embeddedFrame.*\/>/) != -1) {
+        let frameDesc = formattedText.split('<embeddedFrame ')[1].split('/>')[0];
+        let src = frameDesc.split('src=\'')[1].split('\'')[0];
+        let link = frameDesc.split('link=\'')[1].split('\'')[0];
+        let formatedHTMLT = getIframeHTML(src, link);
+
+        formattedText = formattedText.replace(/<embeddedFrame.*?\/>/, formatedHTMLT);
+    }
+
+
     return formattedText;
 }
 
 function getPanelHtml(type) {
     return `<div class="panel-m panel-${type}-m"><p>`;
+}
+
+function getIframeHTML(src, link) {
+    let id = Math.round(Math.random() * 1000000000000);
+    let html = `<div class="custom-code-d">`
+        + `<iframe src="${src}" class="code-iframe-i" id=${id} />`
+        + `<div class="custom-code-buttons" style="text-align: center;">`
+            + `<button type="button" onclick="window.open('${link}', '_blank');" class="btn btn-outline-secondary">Voir le code source</button>`
+            + `<button type="button" onclick="document.getElementById('${id}').src='${src}'" class="btn btn-outline-danger">Remettre à zéro</button>`
+        + `</div>`
+        + `</div>`
+    ;
+
+    return html;
 }
